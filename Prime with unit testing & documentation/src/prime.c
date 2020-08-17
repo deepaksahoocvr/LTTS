4caa6a5641b9b3f16a85666eb33d8ea6eae08c10
deepak sahoo <45771187+deepaksahoocvr@users.noreply.github.com>
2020-08-18 00:18:33 +0530

    Create prime.c

---
+++ Prime with unit testing & documentation/src/prime.c
@@ -0,0 +1,19 @@
+int isprime(int n)
+{
+    if(n==1 || n==0)
+    {
+        return 0;
+
+    }
+    for(int i=2;i<n;i++)
+    {
+
+        if(n%i==0)
+        {
+
+            return 0;
+        }
+    }
+    return 1;
+
+}
