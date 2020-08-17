503fd5b4063a41e71bc2a96c357d23c3f1c1fb71
deepak sahoo <45771187+deepaksahoocvr@users.noreply.github.com>
2020-08-17 21:24:47 +0530

    Create prime_number.c

---
+++ Prime with test and documentation/src/prime_number.c
@@ -0,0 +1,19 @@
+char *isprime(int n)
+{
+    if(n==1 || n==0)
+    {
+        return "False";
+
+    }
+    for(int i=2;i<n;i++)
+    {
+
+        if(n%i==0)
+        {
+
+            return "False";
+        }
+    }
+    return "True";
+
+}
